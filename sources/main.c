8b12b5804a0a6bec3bf9d35f7bff05fe2dcad6b5
ChosenOne1337 <vladcheremnov@gmail.com>
2018-02-03 02:33:20 +0700

    Clean up the working directory

---
+++ sources/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers/calculator.h"
+
+static int ERROR_BITS = 0;
+
+int main(void)
+{
+    String expr = NULL;
+    int loop = 1;
+    double val = 0.0;
+    while (loop) {
+        system("cls");
+        printf("Enter an arithmetic expression:\nEXPR: ");
+        expr = readString();
+        val = calculate(expr, &ERROR_BITS);
+        if (!ERROR_BITS) {
+            printf("ANSWER: %.5lg", val);
+        }
+        else {
+            if (ERROR_BITS & InvalidExprError) {
+                printf("Error: invalid expression!\n");
+            }
+            if (ERROR_BITS & ParenthesesDisbalance) {
+                printf("Error: parentheses disbalance!\n");
+            }
+            if (ERROR_BITS & DivZeroError) {
+                printf("Error: division by zero!\n");
+            }
+            if (ERROR_BITS & MallocError) {
+                printf("Error: memory allocation failure!\n");
+            }
+        }
+        destroyString(expr);
+        printf("\nTry again?\n");
+        scanf("%d", &loop);
+        getchar(); //discards a trailing newline character
+    }
+    return 0;
+}
