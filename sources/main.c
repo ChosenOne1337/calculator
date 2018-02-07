58211e0edcef95d44f50c8a18b454d74b6452630
ChosenOne1337 <vladcheremnov@gmail.com>
2018-02-07 21:54:06 +0700

    Remake CLI

--- sources/main.c
+++ sources/main.c
@@ -1,76 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
 #include "calculator.h"
 
-double calc(const char *expr, FILE *file) {
-    Error error = {0, NULL};
-    double val = 0.0;
-    if (expr == NULL) {
-        if (file == NULL) {
-            return 0.0;
-        }
-        char *str = readString(file);
-        val = calculate(str, &error);
-        destroyString(str);
-    }
-    else {
-        val = calculate(expr, &error);
-    }
+void calc(FILE *pInput, FILE *pOutput) {
+    Error error = {.isError = 0, .msg = NULL};
+    char *str = readString(pInput);
+    double val = calculate(str, &error);
+    destroyString(str);
     if (error.isError) {
         fprintf(stderr, "%s\n", error.msg);
+        return;
     }
-    return val;
+    fprintf(pOutput, "%.5lg\n", val);
 }
 
-const char *getFileName(const char *path) {
-    //returns pointer to the beginning of the file name
-    char *ptr = strrchr(path, '/');
-    if (ptr == NULL) {
-        ptr = strrchr(path, '\\');
-        if (ptr == NULL) {
-            return path;
-        }
+void calcFile(FILE *pInput, FILE *pOutput) {
+    if (pInput == stdin) {
+        calc(stdin, pOutput);
+        return;
     }
-    return ptr + 1;
+    while (!feof(pInput)) {
+        calc(pInput, pOutput);
+    }
+}
+
+// command-line interface
+
+typedef enum Option {
+    HelpOption,
+    FileOption,
+    TotalOptions
+} Option;
+
+static const char *options[TotalOptions] = {"-h", "-f"};
+
+void printUsage(char *appPath) {
+    char *appName = basename(appPath);
+    printf("Usage:\n"
+           "%s: read expression from the standard input,\n"
+           "%s [-f] f1 f2: read expressions from the f1, put results to the f2\n",
+           appName, appName);
 }
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
-        //read from the stdin
-        printf("%.5lg", calc(NULL, stdin));
+        //stdin
+        calcFile(stdin, stdout);
         return 0;
     }
-    if (!strcmp(argv[1], "-f") && argc >= 4) {
-        //file mode
+    if (!strcmp(argv[1], options[HelpOption])) {
+        //usage information
+        printUsage(argv[0]);
+        return 0;
+    }
+    if (!strcmp(argv[1], options[FileOption])) {
+        //read from files
+        if (argc < 4) {
+            printUsage(argv[0]);
+            return 0;
+        }
         FILE *pInput = fopen(argv[2], "r");
-        FILE *pOutput = fopen(argv[3], "w");
         if (pInput == NULL) {
-            printf("Failed to open %s!", argv[2]);
+            fprintf(stderr, "Error: Failed to open %s!", argv[2]);
             return 1;
         }
+        FILE *pOutput = fopen(argv[3], "w");
         if (pOutput == NULL) {
-            printf("Failed to open %s!", argv[3]);
+            fprintf(stderr, "Error: Failed to open %s!", argv[3]);
             return 1;
         }
-        while (!feof(pInput)) {
-            fprintf(pOutput, "%.5lg\n", calc(NULL, pInput));
-        }
+        calcFile(pInput, pOutput);
         fclose(pInput);
         fclose(pOutput);
         return 0;
     }
-    if (argc == 2 && strcmp(argv[1], "-h") != 0) {
-        //expression
-        printf("%.5lg", calc(argv[1], NULL));
-        return 0;
-    }
-    //-h, --help or incorrect argument list
-    char *appName = getFileName(argv[0]);
-    printf("Usage:\n"
-           "%s: read expression from the standard input,\n"
-           "%s \"expr\": calculate expression in quotes,\n"
-           "%s [-f] f1 f2: read expressions from the f1, put results to the f2\n",
-           appName, appName, appName);
+    printUsage(argv[0]);
     return 0;
 }
